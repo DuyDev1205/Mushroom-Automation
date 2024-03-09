16b058d84fa93ed0213ebb0c69466dbe48c68edc
duc <taduc1130@gmail.com>
2024-03-09 22:05:39 +0700

    dht11 with esp8266

--- MushroomAutomation/MushroomAutomation.ino
+++ MushroomAutomation/MushroomAutomation.ino
@@ -1,104 +1,29 @@
-#include <SHT3x.h>
-SHT3x Sensor;
+#include <DHT.h>
 
-#include "secret_pass.h"
+#define DHTPIN D2     // Chân kết nối của cảm biến DHT11 với ESP32
+#define DHTTYPE DHT11   // Loại cảm biến (DHT11 hoặc DHT22)
 
-#define BLYNK_PRINT Serial
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
-
-const int pumpPin = 12;
-bool autoControl = true;
-float desiredHumidity = 60.0;
-unsigned long lastSprayTime = 0;
-unsigned long lastCheckTime = 0;
-float currentTemperature, currentHumidity;
-int pumpState = LOW;
-int pumpCount = 0;
-int lastDayPumpCount = 0;
-int lastDay = 0;
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  pinMode(pumpPin, OUTPUT);
   Serial.begin(9600);
-  Sensor.Begin();
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-}
-
-void getTemperatureAndHumidity(float& temperature, float& humidity) {
-  Sensor.UpdateData();
-  temperature = Sensor.GetTemperature();
-  humidity = Sensor.GetRelHumidity();
-}
-
-void autoControlMode(float& humidity) {
-  getTemperatureAndHumidity(currentTemperature, currentHumidity);
-
-  unsigned long currentMillis = millis();
-  
-  if (currentHumidity < humidity) {
-    if (currentMillis - lastSprayTime >= 10000) {
-      digitalWrite(pumpPin, HIGH);
-      Blynk.setProperty(V3, "color", "#2EA5D8");
-      Blynk.virtualWrite(V3, 1); 
-      lastSprayTime = currentMillis;
-      pumpCount++;
-      Blynk.virtualWrite(V7, pumpCount); // Cập nhật số lần bơm
-      lastDayPumpCount++;
-      delay(2000);
-      digitalWrite(pumpPin, LOW);
-      Blynk.setProperty(V3, "color", "#FF0000" );
-      Blynk.virtualWrite(V3, 0); 
-    }
-  } else {
-    digitalWrite(pumpPin, LOW);
-    Blynk.setProperty(V3, "color", "#FF0000" );
-    Blynk.virtualWrite(V3, 0); 
-  }
+  dht.begin();
 }
 
 void loop() {
-  Blynk.run();
-  Blynk.setProperty(V3, "label", "Máy bơm");
-  Blynk.setProperty(V1, "color", "#2EA5D8");
-  Blynk.setProperty(V2, "color", "#2EA5D8");
-  Blynk.setProperty(V4, "color", "#2EA5D8");
-  Blynk.setProperty(V5, "color", "#2EA5D8");
-  Blynk.setProperty(V6, "color", "#2EA5D8");
+  float humidity = dht.readHumidity(); // Đọc độ ẩm từ cảm biến
+  float temperature = dht.readTemperature(); // Đọc nhiệt độ từ cảm biến
 
-  float temperature, humidity;
-  getTemperatureAndHumidity(temperature, humidity);
-  Serial.println(temperature);
-  Serial.println(humidity);
-  Blynk.virtualWrite(V1, temperature);
-  Blynk.virtualWrite(V2, humidity);
-  Blynk.setProperty(V3, "color", pumpState == HIGH ? "#00FF00" : "#FF0000");
-
-  if (autoControl && humidity != 0) {
-    autoControlMode(desiredHumidity);
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
   }
 
-  unsigned long currentTime = millis();
-  int currentDay = currentTime / (1000 * 60 * 60 * 24); // Lấy ngày hiện tại
-  if (currentDay != lastDay) { // Nếu đã qua ngày mới
-    lastDay = currentDay;
-    lastDayPumpCount = pumpCount; // Lưu trữ số lần bơm của ngày hôm trước
-    pumpCount = 0; // Reset số lần bơm
-    Blynk.virtualWrite(V8, lastDayPumpCount); // Cập nhật số lần bơm của ngày hôm trước
-    Blynk.virtualWrite(V7, pumpCount); // Cập nhật số lần bơm
-  }
-}
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print("% - Temperature: ");
+  Serial.print(temperature);
+  Serial.println("°C");
 
-BLYNK_WRITE(V4) {
-  autoControl = param.asInt();
-}
-
-BLYNK_WRITE(V3) {
-    pumpState = param.asInt();
-    digitalWrite(pumpPin, pumpState);
-}
-
-BLYNK_WRITE(V5) {
-  desiredHumidity = param.asFloat();
+  delay(2000); // Đợi 2 giây trước khi đọc lại dữ liệu từ cảm biến
 }
